37469387d21d8265990f7bffcac53caf17f5574f
Lex Zavala <lex.zavala7@gmail.com>
2021-05-19 10:03:05 -0500

    coverted to Ajax call and added loading message

--- js/script.js
+++ js/script.js
@@ -6,38 +6,39 @@ let getOptions = {
         'Content-Type': 'application/json',
     }
 };
+
+
+    $('#container').html("Loading...");
+
+
+
 const getMovies = () => {
-    fetch("https://pointed-ripple-stork.glitch.me/movies", getOptions)
-        .then(resp => resp.json())
-        .then(movies => {
-            console.log(movies);
-            let htmlStr = "";
-            for (let movie of movies) {
-                htmlStr += `
+    $.ajax('https://pointed-ripple-stork.glitch.me/movies').done((movies) => {
+        console.log(movies);
+    setTimeout(function (){
+       let htmlStr = "";
+        for (let movie of movies){
+            htmlStr += `
                     <div class="card m-1" style="width: 18rem;">
                       <img src=${movie.poster} class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h5 class="card-title">${movie.title}</h5>
+                        <h5 class="card-title">${movie.title.toUpperCase()}</h5>
                         <p class="card-text">${movie.plot}.</p>
                       </div>
                       <ul class="list-group list-group-flush">
                         <li class="list-group-item">Directed by: ${movie.director}</li>
-                        <li class="list-group-item">--</li>
-                        <li class="list-group-item">--</li>
+                        <li class="list-group-item">${movie.genre}</li>
+                        <li class="list-group-item">${movie.rating}</li>
                       </ul>
                       <div class="card-body">
                         <a href="#" class="card-link">link</a>
                         <a href="#" class="card-link">link</a>
                       </div>
                     </div>`
-                    
-                    // <div class="bg-warning">
-                    //     <h2>${movie.title}</h2>
-                    //     <p>by: ${movie.director} ${movie.plot}</p>
-                    // </div> ;
-            }
-            $('#container').html(htmlStr);
-        });
+                }
+                $('#container').html(htmlStr);
+    }, 1000);
+    });
 }
 
 getMovies();
