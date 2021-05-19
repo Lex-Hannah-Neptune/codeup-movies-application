8feabc8e7da5f5e44980046f7141ced61597cd02
Hannah Paterka <paterkahannah@gmail.com>
2021-05-19 10:53:08 -0500

    working add movie function

--- js/script.js
+++ js/script.js
@@ -40,5 +40,55 @@ const getMovies = () => {
     }, 1000);
     });
 }
-
 getMovies();
+
+var newTitle = $('#title').value();
+console.log(newTitle);
+var newRating = $('#rating').value();
+
+let newBook = {
+    "title": `${newTitle}`,
+    "rating": `${newRating}`,
+};
+
+let postOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newBook),
+}
+
+$('#button').click(() => {
+    fetch("https://pointed-ripple-stork.glitch.me/movies", postOptions)
+        .then(getMovies);
+
+});
+
+
+
+
+
+
+// $('#button').click( function() {
+//     let htmlStr = ''
+//         `
+//                     <div class="card m-1" style="width: 18rem;">
+//                       <img src=${movie.poster} class="card-img-top" alt="...">
+//                       <div class="card-body">
+//                         <h5 class="card-title">${movie.title.toUpperCase()}</h5>
+//                         <p class="card-text">${movie.plot}.</p>
+//                       </div>
+//                       <ul class="list-group list-group-flush">
+//                         <li class="list-group-item">Directed by: ${movie.director}</li>
+//                         <li class="list-group-item">${movie.genre}</li>
+//                         <li class="list-group-item">${movie.rating}</li>
+//                       </ul>
+//                       <div class="card-body">
+//                         <a href="#" class="card-link">link</a>
+//                         <a href="#" class="card-link">link</a>
+//                       </div>
+//                     </div>`
+//
+//     $('#container').html(htmlStr)
+// })
