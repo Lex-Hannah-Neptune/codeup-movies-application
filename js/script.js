1616cdc1479fc10f6b45743a353b0786c903bded
Hannah Paterka <paterkahannah@gmail.com>
2021-05-20 17:56:13 -0500

    killed it

--- js/script.js
+++ js/script.js
@@ -16,22 +16,23 @@ const getMovies = () => {
         console.log(movies);
     setTimeout(function (){
        let htmlStr = "";
-        for (let movie of movies){
+        for (let movie of movies) {
             htmlStr += `
                  <div id=${movie.id.toString()} class="card m-1" style="width: 18rem;">
                       <img src=${movie.poster} class="card-img-top" alt="...">
                       <div class="card-body">
                         <h5 contenteditable="false" id="title-${movie.id}" class="card-title">${movie.title.toUpperCase()}</h5>
-                        <p contenteditable="false" id="plot-${movie.id}" class="card-text">${movie.plot}.</p>
+                        <p contenteditable="false" id="plot-${movie.id}" class="card-text plot">${movie.plot}.</p>
                       </div>
                       <ul class="list-group list-group-flush">
-                        <li contenteditable="false" id="director-${movie.id}" class="list-group-item director">Directed by: ${movie.director}</li>
+                        <li contenteditable="false" id="director-${movie.id}" class="list-group-item director">${movie.director}</li>
                         <li contenteditable="false" id="genre-${movie.id}" class="list-group-item genre">${movie.genre}</li>
                         <li contenteditable="false" id="rating-${movie.id}" class="list-group-item rating">${movie.rating}</li>
                       </ul>
                       <div class="card-body d-flex">
                         <i class="fas fa-edit editButton" id="editButton-${movie.id}"></i>
                         <button type="button" class="ml-auto delete">Delete</button>
+                        <button type="button" class="ml-auto patch hide saveChanges">Submit</button>
                       </div>
                 </div>`;
 
@@ -57,28 +58,32 @@ const getMovies = () => {
                 let parent = $(this).parent().parent();
 
                 let title = $(this).parent().parent().children().next().children('.card-title').attr('contenteditable', 'true');
-                let plot = $(this).parent().parent().children().next().children().next().attr('contenteditable', 'true');
-                let director = $(this).parent().parent().children().next().next().children('.director').attr('contenteditable', 'true');
+                let plot = $(this).parent().parent().children().next().children().next('.plot').attr('contenteditable', 'true');
+                let director = "Directed by: " + $(this).parent().parent().children().next().next().children('.director').attr('contenteditable', 'true');
                 let genre = $(this).parent().parent().children().next().next().children('.genre').attr('contenteditable', 'true');
                 let rating = $(this).parent().parent().children().next().next().children('.rating').attr('contenteditable', 'true');
-                $(this).next().attr('class', 'ml-auto saveChanges');
-                $(this).next().html('Save');
+                // $(this).next().attr('class', 'ml-auto saveChanges');
+                // $(this).next().html('Save');
 
                 console.log(genre);
                 console.log(rating);
                 console.log(parentID)
 
-                // EDIT FUNCTION
-                $('button.saveChanges').click(function (){
-                    console.log($(this).text());
+                $(this).next().toggleClass('hide')
+                $(this).next().next().toggleClass('hide')
+                // $('.delete').toggleClass('hide')
 
+                // EDIT FUNCTION
+                $('button.saveChanges').click(function () {
+                    console.log($(this).text());
+                    let parentID =  $(this).parent().parent().attr('id');
                     let editThis = {
                         "poster": $(this).parent().parent().children().next().children('.card-title').text(),
-                        "title": "",
-                        "plot": "",
-                        "director": "",
-                        "genre": "",
-                        "rating": "",
+                        "title": $(this).parent().parent().children().next().children('.card-title').text(),
+                        "plot": $(this).parent().parent().children().next().children().next('.plot').text(),
+                        "director":"Directed by: " + $(this).parent().parent().children().next().next().children('.director').text(),
+                        "genre": $(this).parent().parent().children().next().next().children('.genre').text(),
+                        "rating": $(this).parent().parent().children().next().next().children('.rating').text(),
                     };
                     let patchOptions = {
                         method: 'PATCH',
@@ -87,7 +92,7 @@ const getMovies = () => {
                         },
                         body: JSON.stringify(editThis),
                     }
-                    fetch("https://pointed-ripple-stork.glitch.me/books/7", patchOptions)
+                    fetch(`https://pointed-ripple-stork.glitch.me/movies/${parentID}`, patchOptions)
                         .then(getMovies);
                 });
             });
