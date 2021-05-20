20394d5fb2ce535b8c8d10a75cf937dddc5d56fa
Hannah Paterka <paterkahannah@gmail.com>
2021-05-20 14:28:52 -0500

    lateset revisions

--- js/script.js
+++ js/script.js
@@ -18,16 +18,16 @@ const getMovies = () => {
        let htmlStr = "";
         for (let movie of movies){
             htmlStr += `
-                 <div id=${movie.id.toString()}  class="card m-1" style="width: 18rem;">
+                 <div id=${movie.id.toString()} class="card m-1" style="width: 18rem;">
                       <img src=${movie.poster} class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h5 class="card-title">${movie.title.toUpperCase()}</h5>
-                        <p class="card-text">${movie.plot}.</p>
+                        <h5 id="title-${movie.id}" class="card-title">${movie.title.toUpperCase()}</h5>
+                        <p id="plot-${movie.id}" class="card-text">${movie.plot}.</p>
                       </div>
                       <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Directed by: ${movie.director}</li>
-                        <li class="list-group-item">${movie.genre}</li>
-                        <li class="list-group-item">${movie.rating}</li>
+                        <li id="director-${movie.id}" class="list-group-item">Directed by: ${movie.director}</li>
+                        <li id="genre-${movie.id}" class="list-group-item">${movie.genre}</li>
+                        <li id="rating-${movie.id}" class="list-group-item">${movie.rating}</li>
                       </ul>
                       <div class="card-body d-flex">
                         <i class="fas fa-edit editButton" id="editButton-${movie.id}"></i>
@@ -35,52 +35,72 @@ const getMovies = () => {
                       </div>
                 </div>`;
 
-            function patching () {
-                let htmlPatch = ''
-                htmlPatch += `
-<!--                     <div id=${movie.id.toString()}  class="card m-1" style="width: 18rem;">-->
-                          <img src=${movie.poster} class="card-img-top" alt="...">
-                          <div class="card-body">
-                            <input class="card-title" value="${movie.title.toUpperCase()}">
-                            <input class="card-text" value="${movie.plot}.">
-                          </div>
-                          <ul class="list-group list-group-flush">
-                            <input class="list-group-item" value="Directed by: ${movie.director}">
-                            <input class="list-group-item" value="${movie.genre}">
-                            <input class="list-group-item" value="${movie.rating}">
-                          </ul>
-                          <div class="card-body d-flex">
-<!--                            <a href="#" class="card-link">link</a>-->
-                            <i class="fas fa-edit" id="editButton-${movie.id}"></i>
-                            <button type="button" class="ml-auto delete">Delete</button>
-                          </div>
-<!--                    </div>-->`;
-                // $('#patch').html(htmlPatch);
+            $(".editButton").click(function () {
+                var title = $(this).parent().parent().id;
+                console.log(title)
+//                 let htmlPatch = ''
+//                 htmlPatch += `
+// <!--                     <div id=${movie.id.toString()}  class="card m-1" style="width: 18rem;">-->
+//                           <img src=${movie.poster} class="card-img-top" alt="...">
+//                           <div class="card-body">
+//                             <input class="card-title" value="${movie.title.toUpperCase()}">
+//                             <input class="card-text" value="${movie.plot}.">
+//                           </div>
+//                           <ul class="list-group list-group-flush">
+//                             <input class="list-group-item" value="Directed by: ${movie.director}">
+//                             <input class="list-group-item" value="${movie.genre}">
+//                             <input class="list-group-item" value="${movie.rating}">
+//                           </ul>
+//                           <div class="card-body d-flex">
+// <!--                            <i class="fas fa-edit" id="editButton-${movie.id}"></i>-->
+//                             <button type="button" class="ml-auto patch">Submit</button>
+//                           </div>
+// <!--                    </div>-->`;
+//                 // $('#patch').html(htmlPatch);
                 console.log(htmlPatch);
-            }
+                $('body').css('background', 'blue')
+
+            })
 
             // $(document).on(“click”, “.editButton”, function (e) {
             //     let editID = $(this).data(“id”);
             // }
             let movieID = movie.id
-            $("i.editButton").click(function (){
-                let editID = $(this).attr("id")
-            // //
-            //     let parentID =  $(this).parent().parent().attr('id');
-            //     let turnForm = $(this).parent().parent();
-            //     turnForm.html(patching)
-            // // console.log(parentID);
-            // //
-            // //     // emptyThis.empty();
-            // //
-            // //     //// fetch("https://pointed-ripple-stork.glitch.me/books/7", patchOptions)
-            // //     //             //     .then(getMovies);
-            //     patching()
-                console.log(editID);
-            })
+            // $(".editButton").click(function (){
+            //     let editID = $(this).attr("id")
+            // // //
+            // //     let parentID =  $(this).parent().parent().attr('id');
+            // //     let turnForm = $(this).parent().parent();
+            // //     turnForm.html(patching)
+            // // // console.log(parentID);
+            // // //
+            // // //     // emptyThis.empty();
+            // // //
+            // // //     //// fetch("https://pointed-ripple-stork.glitch.me/books/7", patchOptions)
+            // // //     //             //     .then(getMovies);
+            // //     patching()
+            //     console.log(editID);
+            //     $('body').css('background', 'blue')
+            // })
 
             }
             $('#container').html(htmlStr);
+
+            // $(".editButton").click(function (){
+            //     let editID = $(this).attr("id")
+            //     // //
+            //     //     let parentID =  $(this).parent().parent().attr('id');
+            //     //     let turnForm = $(this).parent().parent();
+            //     //     turnForm.html(patching)
+            //     // // console.log(parentID);
+            //     // //
+            //     // //     // emptyThis.empty();
+            //     // //
+            //     // //     //// fetch("https://pointed-ripple-stork.glitch.me/books/7", patchOptions)
+            //     // //     //             //     .then(getMovies);
+            //     //     patching()
+            //     console.log(editID);
+            // })
 
             $('button.delete').click(function() {
                 let parentID =  $(this).parent().parent().attr('id');
