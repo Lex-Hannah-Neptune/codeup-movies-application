ef9b5984e85e082fc5a5991b18d4b05b4eadd143
Lex Zavala <lex.zavala7@gmail.com>
2021-05-19 19:04:29 -0500

    gave the container of each movie an id matching the id of the film on the database

--- js/script.js
+++ js/script.js
@@ -18,31 +18,41 @@ const getMovies = () => {
        let htmlStr = "";
         for (let movie of movies){
             htmlStr += `
-                     <div class="card m-1" style="width: 18rem;">
-                      <img src=${movie.poster} class="card-img-top" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">${movie.title.toUpperCase()}</h5>
-                        <p class="card-text">${movie.plot}.</p>
-                      </div>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Directed by: ${movie.director}</li>
-                        <li class="list-group-item">${movie.genre}</li>
-                        <li class="list-group-item">${movie.rating}</li>
-                      </ul>
-                      <div class="card-body d-flex">
-                        <a href="#" class="card-link">link</a>
-                        <a href="#" class="card-link">link</a>
-                        <button type="button" class="ml-auto delete">Delete</button>
-                      </div>
+                     <div id=${movie.id.toString()}  class="card m-1" style="width: 18rem;">
+                          <img src=${movie.poster} class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">${movie.title.toUpperCase()}</h5>
+                            <p class="card-text">${movie.plot}.</p>
+                          </div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Directed by: ${movie.director}</li>
+                            <li class="list-group-item">${movie.genre}</li>
+                            <li class="list-group-item">${movie.rating}</li>
+                          </ul>
+                          <div class="card-body d-flex">
+                            <a href="#" class="card-link">link</a>
+                            <a href="#" class="card-link">link</a>
+                            <button type="button" class="ml-auto delete">Delete</button>
+                          </div>
                     </div>`
                 }
                 $('#container').html(htmlStr);
+
                 $('.delete').click(() => {
+                    let deleteOptions = {
+                        method: 'DELETE',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        }
+                    };
+
                     var getParent = $(this).html;
                     var test = 'test'
                     console.log(getParent);
                     console.log(test);
-        }) ;
+                    // fetch(`https://pointed-ripple-stork.glitch.me/movies/10`, deleteOptions)
+                    //     .then(getMovies);
+                });
     }, 1000);
     });
 }
@@ -76,32 +86,19 @@ $('#button').click(() => {
 
 });
 
-// DELETE MOVIES FUNCTION
-// $('.button').click(() => {
-//     var getParent = $(this).parent().parent();
-//     var test = 'test'
-//     console.log(getParent);
-//     console.log(test);
-// }) ;
-
-let deleteOptions = {
-    method: 'DELETE',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-};
-
-$('.delete').click(() => {
-    let inputVal = $('#id-to-delete').val();
-    console.log(inputVal);
-    fetch(`https://pointed-ripple-stork.glitch.me/movies/14`, deleteOptions)
-        .then(getMovies);
-});
-
-// $('#uniqueBooks').click(() => {
+//
+// let deleteOptions = {
+//     method: 'DELETE',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     }
+// };
+//
+// $('.delete').click(() => {
 //     let inputVal = $('#id-to-delete').val();
 //     console.log(inputVal);
-//     fetch(`https://pointed-ripple-stork.glitch.me/books/${inputVal}`, deleteOptions)
-//         .then(getBooks);
-// })
+//     fetch(`https://pointed-ripple-stork.glitch.me/movies/14`, deleteOptions)
+//         .then(getMovies);
+// });
+
 
