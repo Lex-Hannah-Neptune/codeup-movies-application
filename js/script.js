8bb83933628eb4930ef99694362f9ebc4571aabc
Hannah Paterka <paterkahannah@gmail.com>
2021-09-14 22:34:44 -0500

    added modal, url definition, replaced much of chaining

--- js/script.js
+++ js/script.js
@@ -7,45 +7,43 @@
 //     }
 // };
 
-// loading message for fun
+// LOADING MESSAGE FOR FUN
 $('#container').html("Loading...");
 let url = 'https://pointed-ripple-stork.glitch.me/movies';
 
 // POPULATES MOVIE CARDS
 const getMovies = () => {
-    // movies in an arbitrary name, stand in for 'data'
+    // 'movies' is arbitrary, stand in for 'data'
     $.ajax('https://pointed-ripple-stork.glitch.me/movies').done((movies) => {
     //    this timeout was set so that the "loading" message could show.... just for fun
-    setTimeout(function () {
+    // setTimeout(function () {
        let htmlStr = "";
         for (let movie of movies) {
             htmlStr += `
-                 <div id=${movie.id.toString()} class="card m-1 mb-5" style="width: 18rem;">
+                 <div id=${movie.id.toString()} class="card m-1 mb-5 p-0" style="width: 18rem;">
                       <img src=${movie.poster} class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h5 contenteditable="false" id="title-${movie.id}" class="card-title">${movie.title.toUpperCase()}</h5>
-                        <p contenteditable="false" id="plot-${movie.id}" class="card-text plot">${movie.plot}</p>
+                        <h5 id="title-${movie.id}" class="title">${movie.title.toUpperCase()}</h5>
+                        <p id="plot-${movie.id}" class="plot">${movie.plot}</p>
+                        <p id="director-${movie.id}" class="director">${movie.director}</p>
+                        <p id="genre-${movie.id}" class="genre">${movie.genre}</p>
                       </div>
-                      <ul class="list-group list-group-flush">
-                        <li contenteditable="false" id="director-${movie.id}" class="list-group-item director">${"Directed By: " + movie.director}</li>
-                        <li contenteditable="false" id="genre-${movie.id}" class="list-group-item genre">${movie.genre}</li>
-                      </ul>
-                      <div contenteditable="false" id="rating-${movie.id} class="rating">
-                            <i class="fas fa-star ml-3"></i>                           
+                      <div id="rating-${movie.id}" class="rating ms-3">
+                            <i class="fas fa-star"></i>                           
                             <i class="fas fa-star"></i>                           
                             <i class="fas fa-star"></i>                         
                             <i class="fas fa-star"></i>                           
                             <i class="fas fa-star"></i>                           
                       </div>
                       <div class="card-body d-flex">
-                        <i class="fas fa-edit editButton" id="editButton-${movie.id}"></i>
-                        <button type="button" class="ml-auto delete">Delete</button>
-                        <button type="button" class="ml-auto patch hide saveChanges">Save</button>
+                        <i class="fas fa-edit editButton me-auto text-center mt-2" id="editButton-${movie.id}"></i>
+                        <button type="button" class="delete me-3">Delete</button>
+                        <button type="button" class="patch hide saveChanges me-3">Save</button>
                       </div>
                 </div>`;
             }
 
-            // putting all the data into the #container div on html page
+            // PUTTING ALL JSON DATA ONTO PAGE
             $('#container').html(htmlStr);
 
             // DELETE FUNCTION
@@ -62,25 +60,24 @@ const getMovies = () => {
             });
 
             // PATCH FUNCTION
-            $('i.editButton').click(function (){
-                let parentID =  $(this).parent().parent().attr('id');
-                let parent = $(this).parent().parent();
-                let title = $(this).parent().parent().find('.card-title').attr('contenteditable', 'true');
-                const plot = $(this).parent().parent().find('.plot').attr('contenteditable', 'true');
-                const director = $(this).parent().parent().find('.director').attr('contenteditable', 'true');
-                const genre = $(this).parent().parent().find('.genre').attr('contenteditable', 'true');
-                const rating = $(this).parent().parent().find('.rating').attr('contenteditable', 'true');
+            $('i.editButton').click(function () {
+                let editItem = $(this).parent().parent();
+                let parentID = editItem.attr('id');
+                let title = editItem.find('.title').attr('contenteditable', 'true');
+                let plot = editItem.find('.plot').attr('contenteditable', 'true');
+                let director = editItem.find('.director').attr('contenteditable', 'true');
+                let genre = editItem.find('.genre').attr('contenteditable', 'true');
+                let rating = editItem.find('.rating').attr('contenteditable', 'true');
 
-                $(this).next().toggleClass('hide')
-                $(this).next().next().toggleClass('hide')
-                $(this).parent().parent().toggleClass('highlight')
-                $(this).parent().prev().children().toggleClass('highlight')
+                editItem.children().children().toggleClass('highlight')
+                $(this).css('visibility', 'hidden');
+                $(this).next().css('visibility', 'hidden');
+                $(this).next().next().toggleClass('hide');
 
                 // EDIT FUNCTION
                 $('.saveChanges').click(function () {
-                    let parentID =  $(this).parent().parent().attr('id');
                     let editThis = {
-                        "poster": $(this).parent().parent().children('img').attr('src'),
+                        "poster": editItem.children('img').attr('src'),
                         "title": title.text(),
                         "plot": plot.text(),
                         "director": director.text(),
@@ -98,26 +95,25 @@ const getMovies = () => {
                         .then(getMovies);
                 });
             });
-    }, 1000);
+    // }, 1000);
     });
 }
 getMovies();
 
 // TODO poster api
 var getPoster = function() {
-    var film = $('#title').value();
-    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?", function (json) {
-    });
+    let film = $('#title').value();
+    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?", function (json) {});
     return false;
 }
 
 // FORM TO ADD NEW ITEMS
 $('#submit').click(() => {
-    var newTitle = $('#title').val();
-    var newRating = $('#rating').val();
-    var newPlot = $('#plot').val();
-    var newDirector = $('#director').val();
-    var newGenre = $('#genre').val();
+    let newTitle = $('#title').val();
+    let newRating = $('#rating').val();
+    let newPlot = $('#plot').val();
+    let newDirector = $('#director').val();
+    let newGenre = $('#genre').val();
     let newMovie = {
         "title": `${newTitle}`,
         "rating": `${newRating}`,
@@ -137,23 +133,7 @@ $('#submit').click(() => {
     getPoster();
 });
 
-// TOGGLES VIEW OF ADD MOVIES FORM, is this still needed now that it's a modal?
-// $("#button").click(() => {
-//     $('#form').toggleClass('hide')
-//     $('#addMovie').toggleClass('visibility', 'hidden' )
-// });
-//
-// $("#addMovie").click(() => {
-//     $('#form').toggleClass('hide')
-//     $('#addMovie').toggleClass('visibility', 'hidden' )
-// });
-//
-// $("#button").click(() => {
-//     $('#form').toggleClass('hide')
-//     $('#addMovie').toggleClass('visibility', 'hidden' )
-// });
-
-// this is a work in progress
+// todo this is a work in progress
 $(".fa-star").click(() => {
     $('.fa-star').toggleClass('yellow')
 });
